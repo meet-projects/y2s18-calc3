aee30ef9469781dc9dbceb3e88de03f1452ed254
Matthew Feng <mattfeng@mit.edu>
2018-07-26 09:30:29 +0300

    Upload solution from lab2 that is now lab3

---
+++ solution/js/calc.js
@@ -0,0 +1,225 @@
+/* START OF STARTER CODE: You should not need to modify these functions. */
+function exponent(base, power) {
+  var ans = 1;
+
+  for (var i = 0; i < power; i++) {
+    ans *= base;
+  }
+
+  return ans;
+}
+
+function calculate(operation, value1, value2) {
+  return operation(value1, value2);
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+function mult(a, b) {
+  return a * b;
+}
+
+function div(a, b) {
+  return a / b;
+}
+
+function mod(a, b) {
+  return a % b;
+}
+/* END OF STARTER CODE */
+
+/* Calculator Logic */
+var result = 0;
+var curValue1 = 0;
+var curValue2 = false;
+var decimalPlace = 0;
+var curOperation = false;
+var curOperationFunction = false;
+var hasDecimalBeenPressed = false;
+
+function updateDisplay(value) {
+  // var valueString = value.toPrecision(10);
+  var valueString = value.toString();
+  var numDigitsOneEm = 30;
+  // Update the fontsize to fit exactly the number of digits.
+  document.getElementById("result").style.fontSize = Math.min(3.5, numDigitsOneEm / valueString.length - 0.3) + "em";
+
+  // Display the value.
+  document.getElementById("result").value = valueString;
+}
+
+function updateValue(number) {
+  var maxValue = 9e10;
+
+  if (curOperation) { // if an operation has been selected, update value2
+    if (curValue2 === false) {
+      curValue2 = 0;
+    }
+
+    if (curValue2 > maxValue) {
+      return;
+    }
+    if (hasDecimalBeenPressed) {
+      decimalPlace++;
+      curValue2 += Math.pow(10, -decimalPlace) * number;
+    } else {
+      curValue2 = 10 * curValue2 + number;
+    }
+    updateDisplay(curValue2);
+  } else {
+    if (curValue1 > maxValue) {
+      return;
+    }
+
+    if (hasDecimalBeenPressed) {
+      decimalPlace++;
+      curValue1 += Math.pow(10, -decimalPlace) * number;
+    } else {
+      curValue1 = 10 * curValue1 + number;
+    }
+    updateDisplay(curValue1);
+  }
+}
+
+function updateDecimal() {
+  hasDecimalBeenPressed = true;
+}
+
+function setOperation(op) {
+  if (curValue2) {
+    return;
+  }
+
+  var validOps = {
+    "+": add,
+    "-": sub,
+    "*": mult,
+    "/": div,
+    "%": mod,
+    "^": exponent
+  };
+
+  if (!(op in validOps)) {
+    console.log('bad')
+    return;
+  }
+
+  curOperation = op;
+  curOperationFunction = validOps[op];
+}
+
+function compute() {
+  if (!curValue2) {
+    return;
+  }
+
+  result = calculate(curOperationFunction, curValue1, curValue2)
+  curValue1 = 0;
+  curValue2 = false;
+  curOperation = false;
+  updateDisplay(result);
+}
+
+function flipSign() {
+  if (curValue2 !== false) {
+    curValue2 = -curValue2;
+    updateDisplay(curValue2);
+  } else {
+    curValue1 = -curValue1;
+    updateDisplay(curValue1);
+  }
+}
+
+function clearValue() {
+  if (curValue2 !== false) {
+    curValue2 = 0;
+  } else  {
+    curValue1 = 0;
+  }
+
+  updateDisplay(0);
+}
+
+/* Event listeners */
+document.getElementById("btn-0").addEventListener("click", function () {
+  updateValue(0);
+});
+
+document.getElementById("btn-1").addEventListener("click", function () {
+  updateValue(1);
+});
+
+document.getElementById("btn-2").addEventListener("click", function () {
+  updateValue(2);
+});
+
+document.getElementById("btn-3").addEventListener("click", function () {
+  updateValue(3);
+});
+
+document.getElementById("btn-4").addEventListener("click", function () {
+  updateValue(4);
+});
+
+document.getElementById("btn-5").addEventListener("click", function () {
+  updateValue(5);
+});
+
+document.getElementById("btn-6").addEventListener("click", function () {
+  updateValue(6);
+});
+
+document.getElementById("btn-7").addEventListener("click", function () {
+  updateValue(7);
+});
+
+document.getElementById("btn-8").addEventListener("click", function () {
+  updateValue(8);
+});
+
+document.getElementById("btn-9").addEventListener("click", function () {
+  updateValue(9);
+});
+
+document.getElementById("btn-decimal").addEventListener("click", function () {
+  updateDecimal();
+});
+
+document.getElementById("btn-add").addEventListener("click", function () {
+  setOperation('+');
+});
+
+document.getElementById("btn-sub").addEventListener("click", function () {
+  setOperation('-');
+});
+
+document.getElementById("btn-mul").addEventListener("click", function () {
+  setOperation('*');
+});
+
+document.getElementById("btn-div").addEventListener("click", function () {
+  setOperation('/');
+});
+
+document.getElementById("btn-mod").addEventListener("click", function () {
+  setOperation('%');
+});
+
+document.getElementById("btn-compute").addEventListener("click", function () {
+  compute();
+});
+
+document.getElementById("btn-neg").addEventListener("click", function () {
+  flipSign();
+});
+
+document.getElementById("btn-clr").addEventListener("click", function () {
+  clearValue();
+});
+
